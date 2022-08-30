16358ddfaa7a9c55c97c4179b0b7bc86b88a9a7c
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-30 19:09:17 +0900

    posts/ 에서 .md 파일을  데이터로  읽을 수 있게 처리 (gray-matter 패키지 설치, getSortedPostsData 함수 작성)

---
+++ lib/posts.ts
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+console.log("process.cwd", process.cwd());
+console.log("postsDirectory", postsDirectory);
+
+export function getSortedPostsData() {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  console.log("fileNames", fileNames);
+
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, "");
+
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id,
+      ...(matterResult.data as { date: string; title: string }),
+    };
+  });
+
+  // Sort posts by date
+  return allPostsData.sort(({ date: a }, { date: b }) => {
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
