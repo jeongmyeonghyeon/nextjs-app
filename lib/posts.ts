c21d3fcf3862a023714586e0c5228bc64fab0f51
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-08-31 12:40:42 +0900

    포스트 데이터 가져와서 보여주기(remark, remark-html)

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 console.log("process.cwd", process.cwd());
@@ -38,4 +40,37 @@ export function getSortedPostsData() {
       return 0;
     }
   });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  console.log("fileNames", fileNames);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostsData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf-8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(remarkHtml)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  // Combine the data with the id and contentHtml
+  return {
+    id,
+    contentHtml,
+    ...(matterResult.data as { date: string; title: string }),
+  };
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,47 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import React from "react";
+import { getAllPostIds, getPostsData } from "../../lib/posts";
+import homeStyles from "../../styles/Home.module.css";
 
-function Post() {
-  return <div></div>;
+function Post({
+  postData,
+}: {
+  postData: {
+    date: string;
+    title: string;
+    contentHtml: string;
+  };
+}) {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={homeStyles.headingXl}>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
+    </div>
+  );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getAllPostIds(); // return: [{params: {id: 'pre-rendering}}, {params...}]
+  return {
+    paths,
+    fallback: false, // 404 페이지로.
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostsData(params?.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
 
 export default Post;
